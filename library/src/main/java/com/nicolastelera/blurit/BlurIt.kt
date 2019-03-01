76818c1ad3511d8f48aa9d3befe1bc0c50e702f0
Nicolas Telera <ntelera@octo.com>
2019-03-01 21:30:23 +0100

    added @RequiresApi annotations

--- library/src/main/java/com/nicolastelera/blurit/BlurIt.kt
+++ library/src/main/java/com/nicolastelera/blurit/BlurIt.kt
@@ -3,15 +3,20 @@ package com.nicolastelera.blurit
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Rect
+import android.os.Build
 import android.renderscript.*
+import android.support.annotation.RequiresApi
 
+@RequiresApi(value = Build.VERSION_CODES.JELLY_BEAN_MR1)
 class BlurIt(context: Context) {
 
     private val renderScript: RenderScript = RenderScript.create(context)
     private val script: ScriptIntrinsicBlur = ScriptIntrinsicBlur.create(renderScript, Element.U8_4(renderScript))
 
+    @RequiresApi(value = Build.VERSION_CODES.JELLY_BEAN_MR2)
     fun blurBitmap(bitmap: Bitmap, intensity: Float): Bitmap = blur(bitmap, intensity)
 
+    @RequiresApi(value = Build.VERSION_CODES.LOLLIPOP)
     fun blurBitmapPart(bitmap: Bitmap, intensity: Float, bounds: Rect): Bitmap {
         val launchOptions = with(bounds) {
             Script.LaunchOptions().apply {
@@ -20,6 +25,11 @@ class BlurIt(context: Context) {
             }
         }
         return blur(bitmap, intensity, true, launchOptions)
+    }
+
+    fun destroy() {
+        script.destroy()
+        renderScript.destroy()
     }
 
     private fun blur(
@@ -48,10 +58,5 @@ class BlurIt(context: Context) {
         outAllocation.destroy()
 
         return output
-    }
-
-    fun destroy() {
-        script.destroy()
-        renderScript.destroy()
     }
 }
